c3376a2685edfd5a3188ab0e3fbe623cc4de6f6a
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-11-11 20:24:21 +0300

    feat: Nine checkout static component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,58 @@
-import Image from "next/image";
+"use client"
+import { Image } from "antd";
+import { AvatarDemo } from "@/components/shared/avatar";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    return (
+        <div className="max-w-[500px] m-auto  text-white">
+            <div className="pt-20 ">
+                <div className="bg-lime-800 rounded-lg ">
+                    <section className="flex justify-start items-center p-4 gap-2">
+                        <AvatarDemo />
+                        <p className= "text-base font-semibold">Pash Ventures</p>
+                    </section>
+                    <section className="flex p-4 gap-2 justify-between items-center">
+                        <p>This section contains the product description. Cute pink shoes
+                        
+                        </p>
+                        <div className="max-w-[200px] ">
+                            <Image
+                                src="https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg"
+                                alt="Picture of the product"
+                                width={100}
+                                height={100}
+                                preview={false}
+                                className="rounded-lg"
+                            />
+                        </div>
+                    </section>
+                    <section className="flex justify-between fonrt-semibold p-4"> 
+                        <p>TOTAL</p>
+                        <p>20 USD</p>
+                    </section>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+                </div>
+               <section className="bg-slate-100 px-3 pb-2">
+               <p className="text-base py-2 text-black">Pay with crypto</p>
+                <Button className="bg-lime-800 w-full">
+                    Pay
+                </Button>
+               </section>
+               <section className="text-black flex items-center justify-end pr-3">
+                <p>Powered by </p>
+                <Image
+                src="/assets/images/nine.png"
+                width={50}
+                preview={false}
+                />
+                <section className="font-semibold">
+                    <p>Nine</p>
+                    <p>Network</p>
+                </section>
+               </section>
+
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 export default {
     darkMode: ["class"],
@@ -58,5 +59,6 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), new MiniCssExtractPlugin(),
+  ],
 } satisfies Config;
